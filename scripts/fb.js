e88ea84f1df9e983bf5987968e5b2779c94c8c42
Yehor Teslenko <yehor.teslenko@proxet.com>
2023-05-08 17:55:42 +0300

    fixes

--- scripts/fb.js
+++ scripts/fb.js
@@ -2,26 +2,27 @@ import Script from "next/script";
 import { useState, useEffect } from "react";
 
 export const applyFBScript = () => {
-    const [px, setPx] = useState(null);
-    const [imgUrl, setImgUrl] = useState(
-        `https://www.facebook.com/tr?id=${px}&ev=PageView&noscript=1`
-    );
+    const [pxSettings, setPxSettings] = useState({
+        px: null, imgUrl: null
+    });
 
-    if (typeof window !== "undefined") {
-        const urlParams = new URLSearchParams(window.location.search);
-        if (!px) {
-            setPx(urlParams.get("px"));
-        }
-    }
 
     useEffect(() => {
-        setImgUrl(`https://www.facebook.com/tr?id=${px}&ev=PageView&noscript=1`);
-    }, [px]);
+        if (typeof window !== "undefined") {
+            const urlParams = new URLSearchParams(window.location.search);
+
+            setPxSettings({
+                    px:  urlParams.get("px"),
+                    imgUrl: `https://www.facebook.com/tr?id=${urlParams.get("px")}&ev=PageView&noscript=1`
+                });
+        }
+    }, []);
 
     return (
-        <>
-            <Script id="fb">
-                {`!function(f,b,e,v,n,t,s)
+        pxSettings.px && pxSettings.imgUrl && (
+            <>
+                <Script id="fb">
+                    {`!function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -29,17 +30,17 @@ export const applyFBScript = () => {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', ${px});
+        fbq('init', ${pxSettings.px});
         fbq('track', 'PageView');`}
-            </Script>
+                </Script>
 
-            <img
-                id="fb-img"
-                height="1"
-                width="1"
-                style={{ display: "none" }}
-                src={imgUrl}
-            />
-        </>
-    );
+                <img
+                    id="fb-img"
+                    height="1"
+                    width="1"
+                    style={{display: "none"}}
+                    src={pxSettings.imgUrl}
+                />
+            </>) )
+
 };
